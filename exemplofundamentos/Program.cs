207ec5a6bec2f8413204a6a9d3f4a6371c840610
LincolnJardim <linconl11@hotmail.com>
2026-01-21 15:27:14 -0300

    continuação do curso de c#, aprendi sobre sintaxe básica e tipos de dados

--- exemplofundamentos/Program.cs
+++ exemplofundamentos/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Teste");
+﻿using exemplofundamentos.Models;
+
+
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy"));
+
+
+
+
+
+
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.Nome = "Lincoln";
+// pessoa1.Idade = 27;
+// pessoa1.Apresentar();
+
+// string apresentacao = "Olá, seja bem vindo";
+
+// int quantidade = 1;
+
+// double altura = 1.80;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da variável preco: " + preco);
+// Console.WriteLine("Valor da variável condição: " + condicao);
