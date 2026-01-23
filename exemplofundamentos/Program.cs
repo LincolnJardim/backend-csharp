11e76f51cf17260ea554968fe40d9a730f3ba09f
LincolnJardim <linconl11@hotmail.com>
2026-01-23 11:51:53 -0300

    Operadores condicionais.

--- exemplofundamentos/Program.cs
+++ exemplofundamentos/Program.cs
@@ -1,22 +1,98 @@
 ﻿using exemplofundamentos.Models;
 
 
-int quantidadeEmEstoque = 10;
-int quantidadeCompra = 4;
-bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+Console.WriteLine("Digite uma letra: ");
+string letra = Console.ReadLine();
 
-Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
-Console.WriteLine($"Quantidade da compra: {quantidadeCompra}");
-Console.WriteLine($"É possível realizar a venda?  {possivelVenda}");
+switch (letra)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("Vogal");
+        break;
 
-if (possivelVenda)
-{
-    Console.WriteLine("Venda realizada.");
+    default:
+        Console.WriteLine("Não é uma vogal");
+        break;
 }
-else
-{
-    Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.");
-}
+
+
+
+// if (letra == "a" ||
+//     letra == "e" ||
+//     letra == "i" ||
+//     letra == "o" ||
+//     letra == "u")
+// {
+//     Console.WriteLine("Vogal");
+// }
+// else
+// {
+//     Console.WriteLine("Não é uma vogal");
+// }
+
+
+
+// if (letra == "a")
+// {
+//     Console.WriteLine("Vogal");
+// }
+// else if (letra == "e")
+// {
+//     Console.WriteLine("Vogal");
+// }
+// else if (letra == "i")
+// {
+//     Console.WriteLine("Vogal");
+// }
+// else if (letra == "o")
+// {
+//     Console.WriteLine("Vogal");
+// }
+// else if (letra == "u")
+// {
+//     Console.WriteLine("Vogal");
+// }
+// else
+// {
+//     Console.WriteLine("Não é uma vogal");
+// }
+
+
+
+
+
+
+
+
+
+
+
+// int quantidadeEmEstoque = 10;
+// int quantidadeCompra = 4;
+// bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
+
+// Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+// Console.WriteLine($"Quantidade da compra: {quantidadeCompra}");
+// Console.WriteLine($"É possível realizar a venda?  {possivelVenda}");
+
+// if (quantidadeCompra == 0)
+// {
+//     Console.WriteLine("Venda inválida.");
+// }
+
+// else if (possivelVenda)
+// {
+//     Console.WriteLine("Venda realizada.");
+// }
+
+// else
+// {
+//     Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.");
+// }
 
 
 
--- exemplofundamentos/obj/Debug/net10.0/exemplofundamentos.AssemblyInfo.cs
+++ exemplofundamentos/obj/Debug/net10.0/exemplofundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exemplofundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+207ec5a6bec2f8413204a6a9d3f4a6371c840610")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebc0b233f3a5655782cefc5c3d6368865b35ff41")]
 [assembly: System.Reflection.AssemblyProductAttribute("exemplofundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exemplofundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
