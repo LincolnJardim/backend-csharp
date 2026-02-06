0eb75aa553c26e2155a608993b000a1c363dae2f
Lincoln Jardim <linconl11@hotmail.com>
2026-02-06 15:38:17 -0300

    aprendendo sobre propriedades, métodos e construtores.

---
+++ ExemploExplorando/Program.cs
@@ -0,0 +1,19 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa(nome: "Lincoln", sobrenome: "Jardim");
+Pessoa p2 = new Pessoa(nome: "Eduardo", sobrenome: "Neves Queiroz");
+
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Lincoln";
+// p1.Sobrenome = "Jardim";
+// p1.Idade = 27;
+// p1.Apresentar();
